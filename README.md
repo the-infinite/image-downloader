f110109bbf3e2603c3b4309a44e5684578f37b18
the-infinite <toby.johnson.precious@gmail.com>
2023-11-18 15:34:04 +0100

    Trimmed out execution fat.

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # com.reliance.tobi.test.imageloader
-A Flutter project that demonstrates the use of Dart isolates to download mutiple images in parallel and then show them on the screen. The intention behind using Dart isolates in favour of regular async-await syntax is to prevent the UI thread from blocking while the network is fetching the image in question.
+A Flutter project that demonstrates the use of Dart isolates to download multiple images in parallel and then show them on the screen. The intention behind using Dart isolates in favour of regular async-await syntax is to prevent the UI thread from blocking while the network is fetching the image in question.
 
 # Theory
-The Dart runtime uses an event-oriented and single-threaded model to acheive concurrency. This means that the default way the runtime acheives concurrency in Dart is by:
+The Dart runtime uses an event-oriented and single-threaded model to achieve concurrency. This means that the default way the runtime achieves concurrency in Dart is by:
 
-1) Executinng the app until it encounters a blocking function.
+1) Executing the app until it encounters a blocking function.
 2) When this blocking function blocks, the runtime executes the rest of the code in the context of execution surrounding the blocking method call, and juggles this with the blocking method itself. That way it is doing them at the same time.
-3) When one of the executing parts of the program (it might be the blockin function call or the code around it), is done running, the program would move forward.
+3) When one of the executing parts of the program (it might be the blocking function call or the code around it), is done running, the program would move forward.
 
 In Dart, these blocking function calls are `Future`s. A Future is similar to a JavaScript promise in the sense that it is an event-driven model of a concurrent process. Much like a JavaScript promise, a `Future` in Flutter has methods to wait for events while executing. One such function is the `Future<T>.then(T value)` function that executes right after the concurrent process is finished.
+
+# Running
+To run this app, make sure you have flutter installed and if you do not, you can get it from [here](https://flutter.dev), and follow all the instructions they give you for your operating system. Pull this repository from GitHub into a new folder.
+
+After you pull the repository and make sure you have flutter installed, run the following command to built it for an android device as a target:
+```sh
+flutter build apk --release
+```
+
+When you have built this app, send it to your target android device and then you
--- lib/home.dart
+++ lib/home.dart
@@ -18,76 +18,70 @@ class HomePage extends StatelessWidget {
 
     return Scaffold(
         body: SafeArea(
-            child: SizedBox(
-            height: size.height,
-            width: size.width,
-            child: Expanded(
-                child: Stack(
-                    alignment: Alignment.bottomCenter,
-                    children: [
-                      SizedBox(
-                        height: size.height,
-                        width: size.width,
-                        child: ValueListenableBuilder(
-                          valueListenable: listNotifier,
-                          builder: (context, list, index) {
-                            // Return this as you see it.
-                            return GridView(
-                              addAutomaticKeepAlives: false,
-                              padding: const EdgeInsets.all(10),
-                              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                crossAxisSpacing:10,
-                                mainAxisSpacing: 10,
-                              ),
-                              children: list,
-                            );
-                          }
-                        )
-                      ),
-
-                      TextButton(
-                        onPressed: () {
-                            final list = List<Widget>.empty(growable: true);
-
-                            // First, add everything in the complete list.
-                            if(completeList.isNotEmpty) {
-                                list.addAll(completeList);
-                            }
-
-                            // Add 7 items to it then...
-                            for(int i = 0; i < 7; i++) {
-                                // Build the current item
-                                final item = UtilityImageView(ValueNotifier<Uint8List?>(null));
-
-                                // Add it to the list of items.
-                                list.add(item);
-
-                                // Add it to the overall list of item.
-                                completeList.add(item);
-                            }
-
-                            // This is our new list.
-                            listNotifier.value = list;
-                        },
-
-                        child: Container(
-                          padding: const EdgeInsets.only(top: 15, bottom: 15, left: 30, right: 30),
-                          decoration: BoxDecoration(
-                            color: Colors.blue.shade200,
-                            borderRadius: BorderRadius.circular(12)
+            child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  SizedBox(
+                    height: size.height,
+                    width: size.width,
+                    child: ValueListenableBuilder(
+                      valueListenable: listNotifier,
+                      builder: (context, list, index) {
+                        // Return this as you see it.
+                        return GridView(
+                          addAutomaticKeepAlives: false,
+                          padding: const EdgeInsets.all(10),
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing:10,
+                            mainAxisSpacing: 10,
                           ),
-                          child: const Text(
-                            "Load more",
-                            style: TextStyle(
-                                color: Colors.white,
-                            )
-                          )
+                          children: list,
+                        );
+                      }
+                    )
+                  ),
+
+                  TextButton(
+                    onPressed: () {
+                        final list = List<Widget>.empty(growable: true);
+
+                        // First, add everything in the complete list.
+                        if(completeList.isNotEmpty) {
+                            list.addAll(completeList);
+                        }
+
+                        // Add 7 items to it then...
+                        for(int i = 0; i < 7; i++) {
+                            // Build the current item
+                            final item = UtilityImageView(ValueNotifier<Uint8List?>(null));
+
+                            // Add it to the list of items.
+                            list.add(item);
+
+                            // Add it to the overall list of item.
+                            completeList.add(item);
+                        }
+
+                        // This is our new list.
+                        listNotifier.value = list;
+                    },
+
+                    child: Container(
+                      padding: const EdgeInsets.only(top: 15, bottom: 15, left: 30, right: 30),
+                      decoration: BoxDecoration(
+                        color: Colors.blue.shade200,
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      child: const Text(
+                        "Load more",
+                        style: TextStyle(
+                            color: Colors.white,
                         )
                       )
-                    ],
-                )
-              ),
+                    )
+                  )
+                ],
             )
           )
         );
